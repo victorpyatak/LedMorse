046f69b2fd0a2026f85ec30f36d8103124123543
Pyavic <pyatakvictor@gmail.com>
2018-09-29 02:22:40 +0300

    second commit

--- morseLed.ino
+++ morseLed.ino
@@ -1,50 +1,39 @@
+int led = 8;
+
 void setup(){
   Serial.begin(9600);
-  Serial.print("Decode : ");
+  pinMode(led ,OUTPUT);
+}
+void dot(){
+  digitalWrite(led , HIGH);
+  delay(200);
+  digitalWrite(led , LOW);
+  //delay(200);
 }
 
-
-char alpha[] = { 'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z' };
-String morse[] = { ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.." };
-
-
-String toMorse(String , String[]);
-String toMorse(String text , String morse[]){
-  String morseValue ;
-  String letterSpace = "  ";
-  String wordSpace = "   ";
-  for(int k = 0; text[k] ; k++){
-    if(text[k] != ' '){
-      text[k] = toupper(text[k]);
-      letterSpace += morse[text[k] - 'A'] + " ";
-      morseValue = letterSpace;
-      }
-    if(text[k] == ' '){
-      letterSpace += wordSpace;
-    }
-  }
-  return morseValue ;
+void dash(){
+  digitalWrite(led ,HIGH);
+  delay(600);
+  digitalWrite(led , LOW);
+ // delay(200);
 }
 
-
-char readStr = 0;
-String text , morsecode;
-
+void LetterSpace(){
+  delay(200);
+}
+void WordSpace(){
+  delay(600);
+}
 
 void loop(){
-  if(Serial.available() > 0){
-    readStr = Serial.read();
-
-    if(readStr != '\n'){
-      text += readStr;
-    }else{
-      Serial.println(text);
-      Serial.print("Here will go Morse code : ");
-      Serial.println(toMorse(text , morse));
-      text = "";
-      Serial.print("Decode : ");
-
-    }
-  }
-  
+  dash();
+  LetterSpace();
+  dot();
+  LetterSpace();
+  dot();
+  LetterSpace();
+  dot();
+  LetterSpace();
+  dash();
+  LetterSpace();
 }
